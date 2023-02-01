b436eaf5289cbe172030350eb114d60ceec0fbed
Kirill <kirill.brik@yandex.ru>
2023-02-01 21:07:17 +0300

    .

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -36,10 +36,7 @@ async function buy() {
       } else {
         bagStore.addProduct(props.product);
       }
-<<<<<<< HEAD
       inBag.value = !inBag.value;
-=======
->>>>>>> e02d8b18272d83cba4e4d036558a4bda4e9eb821
       return str;
     })
     .catch((err) => {
@@ -48,10 +45,6 @@ async function buy() {
     .finally(() => {
       load.value = false;
     });
-<<<<<<< HEAD
-=======
-  inBag.value = !inBag.value;
->>>>>>> e02d8b18272d83cba4e4d036558a4bda4e9eb821
   alert(data);
 }
 </script>
