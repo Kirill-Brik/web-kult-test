b80917310eef7d70ae7662001bf610c03d4f0c84
Kirill <kirill.brik@yandex.ru>
2023-02-01 21:04:59 +0300

    fix: изменение только при удачном запросе

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -31,6 +31,12 @@ async function buy() {
       for (let key in data) {
         str += `${key}: ${data[key]};\n`;
       }
+      if (inBag.value) {
+        bagStore.removeProduct(props.product.id);
+      } else {
+        bagStore.addProduct(props.product);
+      }
+      inBag.value = !inBag.value;
       return str;
     })
     .catch((err) => {
@@ -39,12 +45,6 @@ async function buy() {
     .finally(() => {
       load.value = false;
     });
-  if (inBag.value) {
-    bagStore.removeProduct(props.product.id);
-  } else {
-    bagStore.addProduct(props.product);
-  }
-  inBag.value = !inBag.value;
   alert(data);
 }
 </script>
